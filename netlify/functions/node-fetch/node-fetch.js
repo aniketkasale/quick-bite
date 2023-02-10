6bb1251503ab1b0f29df25876fe89e3c1217d18f
aniketkasale <aniketkasale02@gmail.com>
2023-02-10 12:07:05 +0530

    removed node-fetch

--- netlify/functions/node-fetch/node-fetch.js
+++
@@ -1,4 +0,0 @@
-const handler = () =>
-  fetch("/.netlify/functions/node-fetch", { headers: { accept: "Accept: application/json" } })
-    .then((x) => x.json())
-    .then(({ msg }) => setMsg(msg))
