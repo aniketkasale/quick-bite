74b3e443ce25d192e0550d55472db9be3281b9b4
aniketkasale <aniketkasale02@gmail.com>
2023-02-10 12:20:08 +0530

    fixing mix-content error

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RESTAURANT_INFO_URL =
-  "http://demo9015506.mockable.io/restaurantInfo/";
+  "https://relaxed-douhua-7a6f8e.netlify.app/restaurantInfo/";
 
-export const RESTAURANTS_URL = "http://demo9015506.mockable.io/getRestaurants";
+export const RESTAURANTS_URL =
+  "https://relaxed-douhua-7a6f8e.netlify.app/getRestaurants";
