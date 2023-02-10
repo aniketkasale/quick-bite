332a4c4bf893d1aa166c688e6d8064154d22ac0a
aniketkasale <aniketkasale02@gmail.com>
2023-02-10 12:40:46 +0530

    fix: resolved  Mixed Content issue

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RESTAURANT_INFO_URL =
-  "http://demo9015506.mockable.io/restaurantInfo/";
+  "https://demo9015506.mockable.io/restaurantInfo/";
 
-export const RESTAURANTS_URL = "http://demo9015506.mockable.io/getRestaurants";
+export const RESTAURANTS_URL = "https://demo9015506.mockable.io/getRestaurants";
