8960343575fab920cb7d61044340f50f3dc042ac
aniketkasale <aniketkasale02@gmail.com>
2023-02-10 12:36:28 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RESTAURANT_INFO_URL =
-  "https://relaxed-douhua-7a6f8e.netlify.app/api/restaurantInfo/";
+  "http://demo9015506.mockable.io/restaurantInfo/";
 
-export const RESTAURANTS_URL =
-  "https://relaxed-douhua-7a6f8e.netlify.app/api/getRestaurants";
+export const RESTAURANTS_URL = "http://demo9015506.mockable.io/getRestaurants";
